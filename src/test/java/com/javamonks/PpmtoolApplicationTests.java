09aa3542d77bc1cb58e24effeaa6aff4e7b97071
Shaelraj <shael.dhum88@gmail.com>
2020-10-02 15:03:01 +0530

    first commit

---
+++ src/test/java/com/javamonks/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javamonks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
