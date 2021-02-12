72c60b5766e436c9136c2020cc5bc35e5a4e6c6d
shaelraj <shael.dhum88@gmail.com>
2021-02-12 08:58:53 +0530

    project structure changed

---
+++ client/ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import CreateProjectButton from "./project/CreateProjectButton";
+import ProjectItem from "./project/ProjectItem";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
+
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+  render() {
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  project: state.project,
+});
+
+export default connect(mapStateToProps, { getProjects })(Dashboard);
